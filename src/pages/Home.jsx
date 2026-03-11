4d9ec08e05659ceb03aaa6add2d77bda15efd09f
geekqueen771 <2chainz131313@gmail.com>
2026-03-11 19:10:37 -0400

    Add Home component for the home page where everyone can post, see posts and chat.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>Here, everyone can post, see posts, and chat!</p>
+        </div>
+    );
+};
+
+export default Home;
